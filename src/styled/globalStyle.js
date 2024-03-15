deb67dd58e240d5cf93bc9ae399d323ff44f9b9f
Taras Kachmar <107129371+TarasKachmar2022@users.noreply.github.com>
2024-03-15 13:52:52 +0200

    update

--- src/styled/globalStyle.js
+++ src/styled/globalStyle.js
@@ -10,9 +10,13 @@ body {
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  background-color: #2f303a;
   background: url(${someBgImage});
   /* background: url('https://pixabay.com/get/g9e985e0db1b946235ec998d3996cbc8dd3c81695ac8d7b36fb0869b89fd0ccd460d0531a229463eff8e66e13faa5a0c484815b5ec50ff03fe31658ffe0853399_1280.jpg') 0 0 / cover no-repeat; */
+  background-size: cover;
   background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
 }
 
 code {
